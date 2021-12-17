5c2f457d4313b118988a79f950749de5b9da50c1
sterlingdesign <16853763+sterlingdesign@users.noreply.github.com>
2021-12-17 15:56:10 -0500

    Updates

--- functions/global-logging.php
+++ functions/global-logging.php
@@ -7,45 +7,45 @@ use Sterling\LogTarget;
 //-------------------------------------------------------------------------------------------------
 function LogEmergency($item, array $context = array())
 {
-  LogTarget::getInstance()->LogAtLevel(LogLevel::EMERGENCY, $item, $context);
+  LogTarget::getInstance()->LogAtLevel(LogLevel::EMERGENCY, $item, $context, true);
 };
 //-------------------------------------------------------------------------------------------------
 function LogAlert($item, array $context = array())
 {
-  LogTarget::getInstance()->LogAtLevel(LogLevel::ALERT, $item, $context);
+  LogTarget::getInstance()->LogAtLevel(LogLevel::ALERT, $item, $context, true);
 };
 //-------------------------------------------------------------------------------------------------
 function LogCritical($item, array $context = array())
 {
-  LogTarget::getInstance()->LogAtLevel(LogLevel::CRITICAL, $item, $context);
+  LogTarget::getInstance()->LogAtLevel(LogLevel::CRITICAL, $item, $context, true);
 };
 //-------------------------------------------------------------------------------------------------
 function LogError($item, array $context = array())
 {
-  LogTarget::getInstance()->LogAtLevel(LogLevel::ERROR, $item, $context);
+  LogTarget::getInstance()->LogAtLevel(LogLevel::ERROR, $item, $context, true);
 };
 //-------------------------------------------------------------------------------------------------
 function LogWarning($item, array $context = array())
 {
-  LogTarget::getInstance()->LogAtLevel(LogLevel::WARNING, $item, $context);
+  LogTarget::getInstance()->LogAtLevel(LogLevel::WARNING, $item, $context, true);
 };
 //-------------------------------------------------------------------------------------------------
 function LogNotice($item, array $context = array())
 {
-  LogTarget::getInstance()->LogAtLevel(LogLevel::NOTICE, $item, $context);
+  LogTarget::getInstance()->LogAtLevel(LogLevel::NOTICE, $item, $context, true);
 };
 //-------------------------------------------------------------------------------------------------
 function LogInfo($item, array $context = array())
 {
-  LogTarget::getInstance()->LogAtLevel(LogLevel::INFO, $item, $context);
+  LogTarget::getInstance()->LogAtLevel(LogLevel::INFO, $item, $context, true);
 };
 //-------------------------------------------------------------------------------------------------
 function LogDebug($item, array $context = array())
 {
-  LogTarget::getInstance()->LogAtLevel(LogLevel::DEBUG, $item, $context);
+  LogTarget::getInstance()->LogAtLevel(LogLevel::DEBUG, $item, $context, true);
 };
 //-------------------------------------------------------------------------------------------------
 function LogAtLevel($level, $item, array $context = array())
 {
-  LogTarget::getInstance()->LogAtLevel($level, $item, $context);
+  LogTarget::getInstance()->LogAtLevel($level, $item, $context, true);
 };
