52f65334f0ab1eecfc7333122371a843850d3b56
sterlingdesign <16853763+sterlingdesign@users.noreply.github.com>
2021-12-17 21:27:43 -0500

    Update LogTarget.php

--- src/LogTarget.php
+++ src/LogTarget.php
@@ -2,6 +2,7 @@
 declare(strict_types=1);
 namespace Sterling;
 
+use parallel\Sync\Error;
 use Psr\Log\LoggerAwareInterface;
 use Psr\Log\LoggerInterface;
 use Psr\Log\LogLevel;
@@ -52,25 +53,63 @@ final class LogTarget implements LoggerAwareInterface
     $message = self::getMessageFromItem($item);
     if(count($context) == 0 && array_search($level, $arLevelsNeedingTrace) !== false)
       $context = self::getContextFromItem($item, $bRemoveCallerFromContext);
+    $strContext = "";
+    if($this->m_bStoreInMemory || !is_object($this->m_oLogger))
+      $strContext = self::formatContext($context);
     if($this->m_bStoreInMemory)
-      $this->storeLog($level, $message, $context);
+      $this->storeLogEntry($level, $message, $strContext);
     if(is_object($this->m_oLogger))
       $this->m_oLogger->log($level, $message, $context);
     else
-      self::sendToPhpLog($level, $message, $context);
+      self::sendToPhpLog($level, $message, $strContext);
   }
 //-------------------------------------------------------------------------------------------------
-  private function storeLog(string $level, string $message, array $context)
+  private function storeLogEntry(string $level, string $message, string $context)
   {
-    $level = strtoupper($level);
-    if(!isset($this->m_arLog[$level]))
-      $this->m_arLog[$level] = array();
-    array_push($this->m_arLog[$level], ["message"=>$message, "context"=>$context]);
+    if(function_exists('hrtime'))
+      {
+      $timeType = 'hrtime';
+      $timeVal = hrtime();
+      }
+    else if(function_exists('microtime'))
+      {
+      $timeType = 'microtime';
+      $timeVal = microtime();
+      }
+    else
+      {
+      $timeType = 'time';
+      $timeVal = time();
+      }
+    array_push($this->m_arLog, [$timeType=>$timeVal,"level"=>$level,"message"=>$message, "context"=>$context]);
   }
 //-------------------------------------------------------------------------------------------------
-  public function getLog() : array
+  public function getLogRaw() : array
   {
     return $this->m_arLog;
+  }
+//-------------------------------------------------------------------------------------------------
+  public function getLogFormatted() : string
+  {
+    $strLog = "";
+    foreach($this->m_arLog as $logItem)
+      {
+      if(!is_array($logItem))
+        continue;
+      $timeVal = "";
+      if(isset($logItem['hrtime']))
+        $timeVal = implode(',', $logItem['hrtime']);
+      else if(isset($logItem['microtime']))
+        $timeVal = strval($logItem['microtime']);
+      else if(isset($logItem['time']))
+        $timeVal = strval($logItem['time']);
+      $strLog .= "[{$timeVal}]";
+      $strLog .= "[" . strtoupper($logItem['level']) . "]";
+      $strLog .= $logItem['message'];
+      $strLog .= $logItem['context'];
+      $strLog .= PHP_EOL;
+      }
+    return $strLog;
   }
 //-------------------------------------------------------------------------------------------------
   protected static function getMessageFromItem($E) : string
@@ -115,8 +154,7 @@ final class LogTarget implements LoggerAwareInterface
         $context = $item->getTrace();
       else
         {
-        // we will create an error and get the stack trace from that
-        $e = new \Exception();
+        $e = new \Exception(); // we will create an error and get the stack trace from that
         $context = $e->getTrace();
         // remove this function, LogAtLevel, and one more if $bRemoveCallerFromContext is true
         $context = array_splice($context, ($bRemoveCallerFromContext ? 3 : 2));
@@ -145,56 +183,127 @@ final class LogTarget implements LoggerAwareInterface
     return $level;
   }
 //-------------------------------------------------------------------------------------------------
-  protected static function sendToPhpLog($level, string $message, array $context)
+  protected static function sendToPhpLog($level, string $message, string $strContext)
   {
     $level = strtoupper($level);
-
-    $strContext = "";
-    if(count($context))
-      {
-      $strContext = PHP_EOL . " CONTEXT:" . PHP_EOL;
-      foreach($context as $arItem)
-        {
-        if(is_array($arItem) && (isset($arItem["file"]) || isset($arItem['function'])))
-          {
-          $strItem = " ";
-          if(isset($arItem['file']))
-            $strItem .= "{$arItem['file']}:";
-          if(isset($arItem['line']))
-            $strItem .= "{$arItem['line']}:";
-          if(isset($arItem['class']))
-            $strItem .= $arItem['class'] . "::";
-          if(isset($arItem['function']))
-            $strItem .= $arItem['function'];
-          }
-        else if(!is_object($arItem))
-          {
-          $strItem = " UNEXPECTED ITEM: ";
-          $strItem .= gettype($arItem);
-          if(is_array($arItem))
-            {
-            $strItem .= "[";
-            foreach($arItem as $key=>$val)
-              {
-              $strItem .= "'{$key}'=>";
-              if(is_string($val))
-                $strItem .= "'{$val}'";
-              else
-                $strItem .= gettype($val);
-              }
-            $strItem .= "]";
-            }
-          else
-            $strItem .= strval($arItem);
-          }
-        else
-          $strItem = " UNEXPECTED OBJECT: " . get_class($arItem);
-        $strContext .= $strItem . PHP_EOL;
-        }
-      //$strContext .= print_r($context, true);
-      }
     if(strlen($message) == 0)
-      $message = "(MESSAGE NOT SUPPLIED)";
+      $message = "(NO MESSAGE SUPPLIED)";
     error_log("[{$level}] {$message}{$strContext}", 0);
+  }
+//-------------------------------------------------------------------------------------------------
+  static private function formatContext(array $context) : string
+  {
+  $strContext = count($context) ? PHP_EOL : "";
+    foreach($context as $arItem)
+      $strContext .= self::formatContextItem($arItem) . PHP_EOL;
+    return $strContext;
+  }
+//-------------------------------------------------------------------------------------------------
+  static private function formatContextItem($item) : string
+  {
+    if(is_array($item) && (isset($item["file"]) || isset($item['function'])))
+      {
+      $strItem = " ";
+      if(isset($item['file']))
+        $strItem .= "{$item['file']}:";
+      if(isset($item['line']))
+        $strItem .= "{$item['line']}:";
+      if(isset($item['class']))
+        $strItem .= $item['class'] . "::";
+      if(isset($item['function']))
+        {
+        $strItem .= $item['function'] . "(";
+        if(isset($item['args']))
+          $strItem .= self::formatContextItemArgs($item['args']);
+        $strItem .= ")";
+        }
+      }
+    else
+      $strItem = self::formatContextItemUnknown($item);
+
+    return $strItem;
+  }
+//-------------------------------------------------------------------------------------------------
+static private function formatContextItemArgs($arArgs) : string
+{
+  $s = "";
+  if(is_array($arArgs))
+    {
+    $strArgs = "";
+    foreach($arArgs as $key => $val)
+      {
+      $strArgs .= $s;
+      if(is_object($val))
+        $strArgs .= get_class($val);
+      else if(is_array($val))
+        $strArgs .= self::dumpArgArray($val);
+      else
+        $strArgs .= $val;
+      $s = ", ";
+      }
+    }
+  else if(is_object($arArgs))
+    {
+    $strArgs = "class " . get_class($arArgs);
+    }
+  else
+    {
+    $strArgs = gettype($arArgs);
+    }
+  return $strArgs;
+}
+//-------------------------------------------------------------------------------------------------
+ private static function dumpArgArray(array $arVal) : string
+ {
+   $str = "[";
+   $s = "";
+   foreach($arVal as $key=>$val)
+     {
+     $str .= $s;
+     if(is_string($val))
+       {
+       $str .= "\"";
+       $str .= substr($val, 0, 64);
+       if(strlen($str) > 64)
+         $str .= "...";
+       $str .= "\"";
+       }
+     else if(is_object($val))
+       $str .= get_class($val);
+     else
+       $str .= gettype($val);
+     $s = ",";
+     }
+   $str .= "]";
+   return $str;
+ }
+//-------------------------------------------------------------------------------------------------
+  private static function formatContextItemUnknown($item) : string
+  {
+    if(is_object($item))
+      $strItem = " UNEXPECTED CONTEXT OBJECT: " . get_class($item);
+    else
+      {
+      $strItem = " UNEXPECTED CONTEXT ITEM: ";
+      $strItem .= gettype($item);
+      if(is_array($item))
+        {
+        $strItem .= " = [";
+        $s = "";
+        foreach($item as $key=>$val)
+          {
+          $strItem .= $s . "'{$key}'=>";
+          $strItem .= gettype($val);
+          $s = ",";
+          }
+        $strItem .= "]";
+        }
+      else
+        {
+        $strItem .= strval($item);
+        }
+      }
+
+    return $strItem;
   }
 }
